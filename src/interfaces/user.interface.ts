377802da4449ea1c758cdde5d020d1f9b0c4728a
elefantinhos2 <douglasramires01@gmail.com>
2022-10-24 09:40:10 -0200

    feat: create interface userRequest and user ResponseCreate

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -6,3 +6,17 @@ export interface IUser {
     password: string
     is_active:boolean
 }
+
+export interface IUserRequest {
+    name:     string
+    celphone: string
+    email:    string
+    password: string
+}
+
+export interface IUserResponseCreate {
+    id:         string
+    name:       string
+    email:      string
+    created_at: Date
+}
