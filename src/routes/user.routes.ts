eee9d8a3d471b02b83b6b286f97ff93921cc672c
elefantinhos2 <douglasramires01@gmail.com>
2022-10-24 16:47:29 -0200

    feat: add route retrive user (get)

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express"
 import createUserController from "../controllers/user/createUser.controller";
+import retriveUserController from "../controllers/user/retriveUser.controller";
 
 const user = Router();
 
 user.post("/register", createUserController)
+
+user.get("", retriveUserController)
 
 export default user
