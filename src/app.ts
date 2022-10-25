0ae26373e77d548eae67ff92520b15d395af360f
elefantinhos2 <douglasramires01@gmail.com>
2022-10-25 08:25:07 -0200

    feat: add middleware od error in app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from 'express';
-import user from './routes/user.routes';
-
+import "express-async-errors"
 import "reflect-metadata"
+
+import user from './routes/user.routes';
+import handleAppErrorMiddleware from './middlewares/handleAppErrors.middleware';
 
 
 const app = express();
 app.use(express.json());
 
 app.use("/users", user)
+
+app.use(handleAppErrorMiddleware)
 
 export default app;
