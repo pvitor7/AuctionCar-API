d3f898f7c6d5fc60fddfa42661ec7967c795272b
elefantinhos2 <douglasramires01@gmail.com>
2022-10-25 08:42:02 -0200

    feat: add routes list user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import createUserController from "../controllers/user/createUser.controller";
+import listUserController from "../controllers/user/listUsers.controller";
 import retriveUserController from "../controllers/user/retriveUser.controller";
 
 const user = Router();
@@ -7,5 +8,7 @@ const user = Router();
 user.post("/register", createUserController)
 
 user.get("/:id", retriveUserController)
+
+user.get("", listUserController)
 
 export default user
