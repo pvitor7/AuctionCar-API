510ffa5e790e2f0f989597fe1c0173d5b074aa9b
elefantinhos2 <douglasramires01@gmail.com>
2022-10-25 09:25:39 -0200

    feat: add routes patch for updated user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import createUserController from "../controllers/user/createUser.controller";
 import listUserController from "../controllers/user/listUsers.controller";
 import retriveUserController from "../controllers/user/retriveUser.controller";
+import updateUserController from "../controllers/user/updateUser.controller";
 
 const user = Router();
 
@@ -10,5 +11,7 @@ user.post("/register", createUserController)
 user.get("/:id", retriveUserController)
 
 user.get("", listUserController)
+
+user.patch("/:id", updateUserController)
 
 export default user
