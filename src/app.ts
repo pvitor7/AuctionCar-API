ce7c4eb9ab0793e47baf20cb1a51a1911a0d15bd
paulovitor <pvitor7@hotmail.com>
2022-11-01 13:21:45 -0300

    feat: intregated with frontend

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import "express-async-errors";
 import "reflect-metadata";
+import cors from "cors";
 
 import user from './routes/user.routes';
 import login from './routes/login.routes';
@@ -8,6 +9,7 @@ import handleAppErrorMiddleware from './middlewares/handleAppErrors.middleware';
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", user)
