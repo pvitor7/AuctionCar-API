4ebd2a5ae9431244257a8a710f1998553f0c800c
elefantinhos2 <douglasramires01@gmail.com>
2022-11-02 20:43:45 -0200

    feat: create interfaces vehicles

---
+++ src/interfaces/motor.interface.ts
@@ -0,0 +1,41 @@
+export interface IVehicle {
+    id:          string
+    heading:     string
+    status:      string
+    km:          string
+    price:       string
+    description: string
+    published:   boolean
+    img:         string
+    created_at: Date
+    updated_at: Date
+}
+
+export interface IVehicleRequestCreate {
+    heading:     string
+    status:      string
+    km:          string
+    price:       string
+    description: string
+    published:   boolean
+    img:         string
+    categorie:   string
+}
+
+export interface Categorie {
+    id:        string
+    categorie: string
+}
+
+export interface IVehicleResponseCreate  {
+    id:          string
+    heading:     string
+    status:      string
+    km:          string
+    price:       string
+    description: string
+    published:   boolean
+    img:         string
+    crated_at:   Date
+    categorie: Categorie
+}
