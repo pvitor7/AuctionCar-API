71fe9ac68dc9dbe56cadf0883dd5815ac72fa093
elefantinhos2 <douglasramires01@gmail.com>
2022-11-02 20:33:21 -0200

    feat: new router list category

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express"
 import createCategoryController from "../controllers/category/createCategory.controller"
+import listCategoryController from "../controllers/category/listCategories.controller"
 
 const category = Router()
 
 category.post("", createCategoryController)
+
+category.get("", listCategoryController)
 
 export default category
