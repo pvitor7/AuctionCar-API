f59b7fe617b3853b36477d8c364e16b9337ae4ca
elefantinhos2 <douglasramires01@gmail.com>
2022-11-02 22:15:33 -0200

    feat: new router

--- src/routes/motor.routes.ts
+++ src/routes/motor.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express"
 import createVehicleController from "../controllers/motor/createMotor.controller"
+import listVehicleController from "../controllers/motor/ListMotors.controller"
 
 const motor = Router()
 
 motor.post("", createVehicleController)
+
+motor.get("", listVehicleController)
 
 export default motor
