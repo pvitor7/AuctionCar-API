481f5ef05bf967b1a9b49970c6f9f3a75dd8766d
paulovitor <pvitor7@hotmail.com>
2022-12-27 12:59:45 -0300

    feat: refact service getOffers

--- src/services/offer/getOffers.service.ts
+++ src/services/offer/getOffers.service.ts
@@ -1,12 +1,8 @@
 import OffersRepository from "../../repositories/offer.repository";
-// import VehicleRepository from "../../repositories/vehicle.repository";
 
 const getVehicleOfferService = async (vehicleId: string) => {
-  // const vehicle = await VehicleRepository.findOne(vehicleId);
-  // console.log(vehicle)
-
+  
   const offers = await OffersRepository.find();
-
   return offers;
 };
 
